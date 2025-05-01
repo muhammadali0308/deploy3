2427349d5d55895bfb26c2cf27ea879b6d749916
muhammadali0308 <abdukarimovm.ali12@gmail.com>
2025-05-01 10:19:11 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,254 @@
+import "./App.css"
+import a from "./a.png"
+import b from "./b.png"
+import e from "./e.png"
+import c from "./c.png"
+
+
+
+  return (
+    <>
+  <header class="container">
+       <div class="bosh">
+        <img src={a} alt="" />
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Product</a>
+                </li>
+                <li>
+                    <a href="#">Faq</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+       </div>
+    </header>
+    <main>
+        <section class="ikkinchi">
+         <div class="container">
+            <p>Food app</p>
+            <h1>Why stay hungry when <br />
+                you can order form Bella Onojie</h1>
+                <h2>Download the bella onoje’s food app now on</h2>
+    <div class="btn-ota">
+        <button class="btn">Playstore</button>
+        <button class="btn">App store</button>
+    </div>
+    <div class="hero">
+        <img src={b} alt="" />
+    </div>
+  </div>
+         </section>
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <section class="con">
+            <h1>How the app works</h1>
+        </section>
+      <div class="container">
+        <section class="r4">
+            <img src={c} alt="" />
+            <div class="text">
+                <h3>Create an account</h3>
+                <h2>Create/login to an existing <br />
+                    account to get started</h2>
+                    <h4>An account is created with your email <br />
+                        and a desired password</h4>
+            </div>
+         </section>
+         <section class="r4">
+            <div class="text">
+                <h3>Create an account</h3>
+                <h2>Create/login to an existing <br />
+                    account to get started</h2>
+                    <h4>An account is created with your email <br />
+                        and a desired password</h4>
+            </div>
+            <img src={c} alt="" />
+         </section>
+         <section class="r4">
+           <img src={c} alt="" />
+            <div class="text">
+                <h3>Create an account</h3>
+                <h2>Create/login to an existing <br />
+                    account to get started</h2>
+                    <h4>An account is created with your email <br />
+                        and a desired password</h4>
+            </div>
+         </section>
+      </div>
+
+
+
+
+     <section class="ohirgi">
+<div class="container">
+    <h1>Download the app now.</h1>
+    <p>Available on your favorite store. Start your premium experience now</p>
+    <div class="btn-ota">
+       <button class="btn">Playstore</button>
+       <button class="btn">App store</button>
+   </div>
+</div>
+     </section>
+    </main>
+<footer class="container">
+  <img src={a} alt="" />
+  <div>
+   <img src={e} alt="" />
+   <img src={e} alt="" />
+   <img src={e} alt="" />
+  </div>
+<h1>Copywright 2020 Bella Onojie.com</h1>
+</footer>
+
+
+    </>
+  )
+
+export default App
